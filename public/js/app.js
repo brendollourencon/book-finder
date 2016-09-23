92c24c16f9f3348f479b458549eff2941c179827
mariocsants <mariocsants@gmail.com>
2016-09-23 19:36:34 -0300

    Commit start Frontend

---
+++ public/js/app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by mario-csantos on 21/09/16.
+ */
---
+++ view/header.php
@@ -0,0 +1,6 @@
+<header class="header">
+    <div class="logo">Book Finder</div>
+    <div class="busca"><input type="text"></div>
+    <div class="carrinho">dwedwedwe</div>
+    <div class="perfil">dewdew</div>
+</header>
