79977f904bcc7570eeb7057428d50bb60043850b
mariocsants <mariocsants@gmail.com>
2016-10-22 18:31:59 -0200

    Definição de cor primária Criação do card

--- view/home.php
+++ view/home.php
@@ -10,11 +10,31 @@ $allProduct = $products->getVitrine();
 
 include_once "header.php";
 ?>
+
+<div class="card" style="margin: 74px auto 0; width: 400px">
+    <div class="card-image">
+        <img src="public/images/card.jpg" alt="">
+    </div>
+    <div class="card-header">
+        <div class="title">Title</div>
+        <div class="subtitle">Subtitle</div>
+    </div>
+    <div class="card-content">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem ea eaque facere, natus odit quos repellat sit tempora vitae. Aut cum dolore dolorem esse fuga iure natus quod unde.
+    </div>
+    <div class="card-controls">
+        <button class="btn-icon blue">
+            <i class="material-icons">add_shopping_cart</i>
+        </button>
+        <button class="btn blue">Visualizar</button>
+    </div>
+</div>
+
 <div style="margin-top: 64px">
     <button class="btn">NORMAL</button>
     <button class="btn" disabled>Disabled</button>
 
-    <button class="btn raised">Raised</button>
+    <button class="btn raised blue">Raised</button>
     <button class="btn raised" disabled>Disabled</button>
 
     <button class="btn-icon">
@@ -27,6 +47,10 @@ include_once "header.php";
 
     <button class="btn-icon fab">
         <i class="material-icons">face</i>
+    </button>
+
+    <button class="btn-icon fab blue">
+        <i class="material-icons">add</i>
     </button>
 </div>
 
