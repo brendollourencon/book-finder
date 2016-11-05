71ec12b163e7691c2d4b424f9656e93f4c297413
Claudanilo Xavier <claudaniloxavier@gmail.com>
2016-11-05 15:51:54 -0200

    Tela de informações do produto

--- view/produto.php
+++ view/produto.php
@@ -10,18 +10,33 @@ include_once "header.php";
 ?>
 
 <div class="menu-offset">
-    pagina de produto
-    <br>
-    <br>
-    <br>
-    <form action="<?php echo SITE_URL; ?>/carrinho" method="post">
+    <div class="container-produto">
+        <h1>Informações do Livro</h1>
 
-        <input type="hidden" name="id_produto" value="<?php echo $infoProduto->id_produto; ?>">
-        <input type="hidden" name="valor_produto" value="<?php echo $infoProduto->valor; ?>">
-        <p><?php echo $infoProduto->titulo; ?></p>
-        <br>
-        <button type="submit">Comprar</button>
-    </form>
+        <div class="info-produto">
+            <div class="image">
+                <img src="../public/imagens/no-image.png" alt="harry potter">
+            </div>
+            <div class="info">
+                <form action="<?php echo SITE_URL; ?>/carrinho" method="post">
+
+                    <input type="hidden" name="id_produto" value="<?php echo $infoProduto->id_produto; ?>">
+                    <input type="hidden" name="valor_produto" value="<?php echo $infoProduto->valor; ?>">
+
+                    <h2 class="book-title"><?php echo $infoProduto->titulo; ?></h2>
+
+                    <p class="book-description"><span class="bold">Descrição: </span> <?php echo $infoProduto->descricao; ?></p>
+
+                    <p class="book-value"><span class="bold">Valor: </span> <?php echo $infoProduto->valor; ?></p>
+
+                    <div class="action">
+                        <button class="btn raised blue" type="submit">Comprar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
 </div>
 
 <?php include_once "footer.php"; ?>
