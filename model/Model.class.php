6bbbb2220959323165c99d67450fc608e1de640e
brendollourencon <brendol.lourencon@gmail.com>
2016-11-07 17:30:04 -0200

    Novo metodo call

--- model/Model.class.php
+++ model/Model.class.php
@@ -8,30 +8,19 @@ class Model
      * Encapsulamento de métodos
      * @author Brendol L.
      */
-    public function __call($name, $arguments)
+    public function __call($metodo, $parametros)
     {
-        $nameAtribute = strtolower(substr($name, 3));
-        $solicitacao = substr($name, 0, 3);
+        $nomeMetodo = strtolower(substr($metodo,0,3));
+        $valorMetodo = strtolower(substr($metodo,3));
 
-        if ($solicitacao == "get") {
-            // versão php
-            if (gettype($this->$nameAtribute) == "array") {
-                return $this->$nameAtribute[0];
+        if($nomeMetodo === "set"){
+            if(isset($parametros)){
+                $this->$valorMetodo = $parametros[0];
             }
-            return $this->$nameAtribute;
-
-        } elseif ($solicitacao == "set") {
-            // versão php
-            if (gettype($this->$nameAtribute) == "array") {
-                $this->$nameAtribute[0] = $arguments;
-            } else {
-                $this->$nameAtribute = $arguments;
-            }
-
-        } else {
-            throw new Exception('O método ' . $name . ' não existe!');
+        }
+        elseif ($nomeMetodo === "get"){
+            return $this->$valorMetodo;
         }
     }
-
 
 }
